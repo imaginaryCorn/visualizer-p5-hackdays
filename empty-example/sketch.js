4c5a55749bcac416193c0a5148f3d298ef53d5ac
imaginaryCorn <adoak@sailthru.com>
2019-06-13 15:28:14 +1200

    Use a 3D Canvas
    * Apply colour based on volume for each pitch
    
    * use cube size based on relative volume
    
    * Apply moveable mouse camera

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,30 +1,37 @@
-let mic, fft, hue;
+
+let fft;
+function preload(){
+  sound = loadSound('Universes.mp3');
+}
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(1200, 400, WEBGL); // Start a 3D Canvas
   noFill();
-
-  mic = new p5.AudioIn();
-  mic.start();
-  fft = new p5.FFT();
-  fft.setInput(mic);
-  hue = 0;
+  perspective(PI / 3.0, width / height, 0.1, 400); // Place the camera
+  
+  fft = new p5.FFT(); // Fast Fourier Transform from p5.sound library
+  sound.amp(0.5);
+  sound.loop();
 }
 
 function draw() {
-  colorMode(HSL, 360) // Set to Hue mode
-  background(0,0,0,270) // Slightly ghost the spectrum
-  let spectrum = fft.analyze(); // get the current microphone data points
+  orbitControl(0.5,0.5); // Apply the ability to move the camera with mouse
+  colorMode(HSL, 360) // Set colours to Hue Saturation Lightness mode
+  clear();
+  let spectrum = fft.analyze(); // get audio data points
+  translate(0,0,100)
+
   beginShape();
   strokeWeight(1);
-  for (i = 0; i < spectrum.length; i+=20) {
-    if (hue > 360) { // rotate through hue values, reset to 0 at 360
-      hue = 0;
-    } else {
-      hue++;
-    }
-    stroke(hue,200,200) // colorize the spectrum
-    curveVertex(i, map(spectrum[i], 0, 255, height, 0));
+  for (i = 0; i < spectrum.length; i+=1) {
+    stroke(map(spectrum[i], 0, 255, 70, 360),200,200) // colorize the spectrum
+    let x = map(i, 0, spectrum.length, -width/2,width/2); 
+    //line(x, height, x, map(spectrum[i], 0, 255, height/2, -height/2))
+    push();
+    translate(x, 0); // Move box placement for each audio frequency
+    // Draw box at a size relating to the volume of the current frequency:
+    box(1, map(-spectrum[i], 0, 255, 0, height/2)); 
+    pop();
   }
   endShape();
 }
